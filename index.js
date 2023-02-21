5dba64c16c43b823b40100344a0ccf8f1ccff13d
AlannaYL <a04810254@gmail.com>
2023-02-21 14:04:50 +0800

    upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import https from 'https'
 import userRoute from './routes/users.js'
 import exhibitionsRoute from './routes/exhibitions.js'
 import orderRoute from './routes/orders.js'
@@ -35,13 +36,15 @@ app.use('/exhibitions', exhibitionsRoute)
 app.use('/orders', orderRoute)
 app.use('/abouts', aboutRoute)
 
-app.get('/', (req, res) => {
-  res.status(200).json({ success: true, message: '' })
-})
-
 app.all('*', (req, res) => {
   res.status(404).json({ success: false, message: '找不到' })
 })
 app.listen(process.env.PORT || 4000, () => {
   console.log('伺服器啟動')
 })
+
+if (process.env.RENDER) {
+  setInterval(() => {
+    https.get(process.env.RENDER)
+  }, 1000 * 60 * 5)
+}
